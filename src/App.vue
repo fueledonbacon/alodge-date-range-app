6c6b1edb6ed216e18a4e13820370f32db1819b81
Ryan Cwynar <rtcx86@gmail.com>
2020-05-07 21:21:53 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <date-picker
+      :minNights="1"
+      @check-in-changed="from = $event"
+      @check-out-changed="to = $event"
+      />
+    <button class="cta" @click="submit">Check Dates</button>
+  </div>
+</template>
+
+<script>
+import DatePicker from 'vue-hotel-datepicker'
+export default {
+  name: 'App',
+  components: {
+    DatePicker
+  },
+  computed: {
+    hash(){
+      const { formatDate } = this
+      return `#checkin=${formatDate(this.from)}&checkout=${formatDate(this.to)}`
+    }
+  },
+  data(){
+    return {
+      from: new Date(),
+      to: new Date(),
+    }
+  },
+  methods: {
+    formatDate(date){
+        let d = String(date.getUTCDate())
+        let m = String(date.getUTCMonth() + 1)
+        let y = String(date.getUTCFullYear())
+
+        d = d.length == 1 ? `0${d}` : d
+        m = m.length == 1 ? `0${m}` : m
+
+        return [y,m,d].join("-")
+    },
+    submit(){
+        location.href = `/lyons?widget=1${this.hash}`
+    }
+  }
+}
+</script>
+
+<style>
+</style>
